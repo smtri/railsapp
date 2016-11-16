1b11e36a509c324e11e479dab6b1deeaab02fbe2
smtri <skip.a.bit@gmail.com>
2016-11-16 20:35:08 +0200

    new db: postgres

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :email, presence: true, 
+                    uniqueness: true,
+                    length: { minimum: 5, maximum: 200, too_long: "%{count} characters is the maximum allowed", too_short: "%{count} characters is the minimum allowed" }
+  validates :password, presence: true,
+                    length: { minimum: 4, maximum: 20, too_long: "%{count} characters is the maximum allowed", too_short: "%{count} characters is the minimum allowed" }
+end
