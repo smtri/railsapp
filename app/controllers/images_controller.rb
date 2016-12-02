0a27b47424d1483afd3ef828e93fba5449dccba7
smtri <skip.a.bit@gmail.com>
2016-12-02 21:29:46 +0200

    image load fixes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,13 +1,38 @@
 class ImagesController < ApplicationController
+
+  def index
+    @image = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new
+    @image = Image.new
+  end
+
   def create
     @user = User.find(params[:user_id])
-    @image = @user.images.create(image_params)
-    redirect_to user_path(@user)
+    @image = @user.images.new(image_params)
+
+    if @image.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+
+    redirect_to images_path    
   end
 
   private
     def image_params
       params.require(:image).permit(:user)
     end
-
+    
 end
